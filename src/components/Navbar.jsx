450c82897314cc1d537e270bbdfad88beaaeb6a1
Cami Fresch <cami.fresch@hotmail.com>
2023-02-06 16:25:53 -0300

    agrego readme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,34 +4,32 @@ import Cartwidget from './Cartwidget'
 const Navbar = () => {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-dark">
-      <div className="container-fluid">
-        <button className="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="index.html">Quien soy?</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="pages/servicios.html">Servicios</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="pages/contacto.html">Contacto</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="pages/reservar-turno.html">Reservar Turno</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="pages/cuidados.html">Cuidados</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Inicio</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Quien Soy?</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Servicios</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacto</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Reserva tu turno</a>
+        </li>
+      </ul>
+    </div>
+  </div>
       <Cartwidget/>
-    </nav>
+</nav>
+
 
 
     </>
